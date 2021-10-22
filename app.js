4a50bdb6982db7b574dcad9b304cb9ac44ac9611
Pablo Aguilera <pablo.aguilera@cbsinteractive.com>
2021-10-22 15:28:47 -0400

    Creating MongoDB and transactional data

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 //OJO express-graphql dependency change, NOW you should get the (graphqlHTTP) function with destructuring syntax
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+
+const Event = require('./models/event')
 const app = express();
-const events = [];
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -39,28 +41,50 @@ app.use(
           query: RootQuery
           mutation: RootMutation
       }
-    `),
+  `),
 
     rootValue: {
       events: () => {
-        return events;
+        return Event
+        .find()
+        .then(events => {
+          return events.map(event => {
+            return { ...event._doc }
+          });
+        }).catch(err => {
+          console.log(err);
+          throw err;
+        });
       },
       createEvent: (arg) => {
-        const event = {
-          _id: Math.random.toString(),
+        const event = new Event ({
           title: arg.eventInput.title,
           description: arg.eventInput.description,
           price: +arg.eventInput.price,
           date: arg.eventInput.date,
-        };
-        events.push(event);
-        return event;
+        });
+        return event
+        .save()
+        .then(result => {
+          console.log(result);
+          return { ...result._doc };
+        }).catch(err => {
+          console.log(err);
+          throw err;
+        });
       },
     },
     graphiql: true,
   })
 );
 
-app.listen(3000);
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clusteratlas.wnue9.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+).then(() => {
+  app.listen(3000);
+}).catch(err => {
+  console.log(err);
+  throw(err);
+});
 
 console.log("Running a GraphQL API server at localhost:3000/graphql");
